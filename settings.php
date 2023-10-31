2be789c3cdb0877dff00de9f1315d94ea736f86c
kbannyi <kbannyi@custis.ru>
2023-10-31 11:58:45 +0300

    feat: выкинул ненужные настройки, переименовал параметры
    Refs: MODEUSSW-21371

--- settings.php
+++ settings.php
@@ -3,106 +3,36 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
-    $ADMIN->add('tools', new admin_category('manageltiextensions', new lang_string('manage', 'tool_ltiextensions')));
-
-    //===============================
     //Общие настройки
-    $common_settings = new admin_settingpage('common', new lang_string('common', 'tool_ltiextensions'));
+    $common_settings = new admin_settingpage('common', 'Common settings');
+    //Настройки линковки пользователей (выбор идентификатора пользователя, который будет использоваться при синхронизации с Modeus)
+    $auto_link_users_settings = new admin_settingpage('users_sync', 'Users sync settings');
+    //Настройки создания новых курсов
+    $pull_courses_settings = new admin_settingpage('courses_sync', 'Courses sync settings');
 
     if ($ADMIN->fulltree) {
         $setting = new admin_setting_configtextarea(
-            'tool_ltiextensions/platform_settings',
-            new lang_string('platform_settings', 'tool_ltiextensions'),
-            new lang_string('platform_settings_descr', 'tool_ltiextensions'),
+            'tool_modeussync/connection_settings',
+            'Connection config',
+            'JSON format (it is important to use double quotes): { "deploymentid": { "lmsapi": "url" }}',
             "",
             PARAM_RAW
         );
         $common_settings->add($setting);
-    }
 
-    $ADMIN->add('manageltiextensions', $common_settings);
-
-    //===============================
-    //Настройки джоба auto_publish_as_lti_tools
-    $auto_publish_settings = new admin_settingpage('auto_publish_as_lti_tools', new lang_string('auto_publish_as_lti_tools', 'tool_ltiextensions'));
-
-    if ($ADMIN->fulltree) {
-        global $CFG;
-        require_once($CFG->dirroot . '/auth/lti/auth.php');
-
-        $authmodes = [
-            auth_plugin_lti::PROVISIONING_MODE_AUTO_ONLY => get_string('provisioningmodeauto', 'auth_lti'),
-            auth_plugin_lti::PROVISIONING_MODE_PROMPT_NEW_EXISTING => get_string('provisioningmodenewexisting', 'auth_lti'),
-            auth_plugin_lti::PROVISIONING_MODE_PROMPT_EXISTING_ONLY => get_string('provisioningmodeexistingonly', 'auth_lti')
-        ];
-
-        $auto_publish_settings->add(new admin_setting_configselect(
-            'tool_ltiextensions/provisioningmodeinstructor',
-            get_string('provisioningmodeteacherlaunch', 'enrol_lti'),
-            get_string('provisioningmode', 'tool_ltiextensions'),
-            auth_plugin_lti::PROVISIONING_MODE_PROMPT_EXISTING_ONLY,
-            $authmodes
-        ));
-
-        $auto_publish_settings->add(new admin_setting_configselect(
-            'tool_ltiextensions/provisioningmodelearner',
-            get_string('provisioningmodestudentlaunch', 'enrol_lti'),
-            get_string('provisioningmode', 'tool_ltiextensions'),
-            auth_plugin_lti::PROVISIONING_MODE_PROMPT_EXISTING_ONLY,
-            $authmodes
-        ));
-
-        $auto_publish_settings->add(new admin_setting_requiredtext(
-            'tool_ltiextensions/lang',
-            new lang_string('lang', 'tool_ltiextensions'),
-            new lang_string('lang_descr', 'tool_ltiextensions'),
-            'en',
-            PARAM_TEXT
-        ));
-    }
-
-    $ADMIN->add('manageltiextensions', $auto_publish_settings);
-
-    //===============================
-    //Настройки джоба start_lti_sync
-    $start_sync_settings = new admin_settingpage('start_lti_sync', new lang_string('start_lti_sync', 'tool_ltiextensions'));
-
-    if ($ADMIN->fulltree) {
-        require_once(__DIR__ . '/settings/admin_setting_admins.php');
-        $setting = new admin_setting_admins(
-            'tool_ltiextensions/sync_user',
-            new lang_string('sync_user', 'tool_ltiextensions'),
-            new lang_string('sync_user_descr', 'tool_ltiextensions')
-        );
-        $start_sync_settings->add($setting);
-    }
-
-    $ADMIN->add('manageltiextensions', $start_sync_settings);
-
-    //===============================
-    //Настройки джоба auto_link_users
-    $auto_link_users_settings = new admin_settingpage('auto_link_users', new lang_string('auto_link_users', 'tool_ltiextensions'));
-
-    if ($ADMIN->fulltree) {
-        require_once(__DIR__ . '/settings/admin_setting_user_fields.php');
+        require_once __DIR__ . '/settings/admin_setting_user_fields.php';
         $auto_link_users_settings->add(new admin_setting_user_fields());
-    }
-
-    $ADMIN->add('manageltiextensions', $auto_link_users_settings);
-
-    //===============================
-    //Настройки джоба pull_courses
-    $pull_courses_settings = new admin_settingpage('pull_courses', new lang_string('pull_courses', 'tool_ltiextensions'));
-
-    if ($ADMIN->fulltree) {
 
         $pull_courses_settings->add(new admin_settings_coursecat_select(
-            'tool_ltiextensions/default_category',
-            new lang_string('default_category', 'tool_ltiextensions'),
-            new lang_string('default_category_descr', 'tool_ltiextensions'),
+            'tool_modeussync/default_category',
+            'Default category for created courses',
+            'Courses created based on data from Modeus will be placed in this category',
             1
         ));
     }
 
-    $ADMIN->add('manageltiextensions', $pull_courses_settings);
+    $ADMIN->add('tools', new admin_category('managemodeussync', 'Modeus Sync Plugin'));
+    $ADMIN->add('managemodeussync', $common_settings);
+    $ADMIN->add('managemodeussync', $auto_link_users_settings);
+    $ADMIN->add('managemodeussync', $pull_courses_settings);
 }
--- settings/admin_setting_user_fields.php
+++ settings/admin_setting_user_fields.php
@@ -1,37 +1,27 @@
 <?php
 
-use tool_ltiextensions\repository\users_repository;
+use tool_modeussync\repository\users_repository;
 
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Выпадающий список с полями с информацией о пользователе:
+ * Выпадающий список с полями, которые можно использовать в качестве сквозного идентификатора (ExternalPersonId) для синхронизации пользователей:
  * стандартные поля из таблицы user (представлены как user::название_поля (например, user::idnumber));
  * кастомные поля из таблицы user_info_field (представлены как user_info_field::field_id (например user_info_field::1)).
- *
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class admin_setting_user_fields extends admin_setting_configselect
 {
-    /**
-     * Calls parent::__construct with specific arguments
-     */
     public function __construct()
     {
         parent::__construct(
-            'tool_ltiextensions/lti_field_user_id',
-            new lang_string('lti_field_user_id', 'tool_ltiextensions'),
-            new lang_string('lti_field_user_id_descr', 'tool_ltiextensions'),
+            'tool_modeussync/user_sync_field',
+            'The field where the user ExternalPersonId is stored',
+            'This field will be used to synchronize the list of users and their grades with Modeus.',
             'user::idnumber',
             null
         );
     }
 
-    /**
-     * Loads an array of choices for the configselect control
-     *
-     * @return bool always return true
-     */
     public function load_choices()
     {
         if (is_array($this->choices)) {
